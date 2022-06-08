1a5ce39ee0caad42fd1378a9cb848b6cc3c0a844
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-06-08 09:24:20 +0100

    feat-initialized photo urls to scrape from

--- src/js/scraper.js
+++ src/js/scraper.js
@@ -1,8 +1,25 @@
-// This file holds the scaper engine that gets all the urls from free stock images platforms.
+/* This file holds the scaper engine that gets all the urls from free stock images platforms .*/
 
 // Scrape a url for images that are free stock and match image name
-function scraper(imageName){
+function photoScraper(imageName){
+    // store a reference to the object context
+    let _this = this;
+
+    // free stock images website
+    let pixabay = 'https://pixabay.com/photos/';
+    let unsplash = 'https://unsplash.com/';
+    let pexels = 'https://www.pexels.com/';
+    let pikWizard = 'https://pikwizard.com/';
+    let rawPixel = 'https://www.rawpixel.com/free-images';
+    let reShot = 'https://www.reshot.com/free-stock-photos/';
+    let freepik = 'https://www.freepik.com/';
+    let vecteezy = 'https://www.vecteezy.com/free-photos';
+    let morgueFile = 'https://morguefile.com/';
+    let pikJumbo = 'https://picjumbo.com/free-stock-photos/';
+
+    // url array to fill from scraper
+    let imageUrls = [];
     return ;
 
-    // Needed functions: .get(), .getAll(), .download(), .display(),
+    // Needed methods: .get(), .getAll(), .download(), .display(),
 }
